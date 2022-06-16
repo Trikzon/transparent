eacf08ec5d124f7fd224c38958d8bf22432f51f1
Trikzon <me@diontryban.com>
2022-06-16 01:15:17 -0400

    Automatically remove back texture from transparent paintings

--- forge/src/main/java/com/trikzon/transparent/client/forge/TransparentClientForge.java
+++ forge/src/main/java/com/trikzon/transparent/client/forge/TransparentClientForge.java
@@ -1,15 +1,22 @@
 package com.trikzon.transparent.client.forge;
 
+import com.trikzon.transparent.client.TransparentClient;
 import com.trikzon.transparent.client.TransparentConfigReloadListener;
+import com.trikzon.transparent.client.render.forge.SpriteRegistryImpl;
 import net.minecraft.client.Minecraft;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class TransparentClientForge {
     public TransparentClientForge() {
+        TransparentClient.initialize();
+
         ResourceManager resourceManager = Minecraft.getInstance().getResourceManager();
         if (resourceManager instanceof ReloadableResourceManager) {
             ((ReloadableResourceManager) resourceManager).registerReloadListener(new TransparentConfigReloadListener());
         }
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(SpriteRegistryImpl::onTextureStitchPre);
     }
 }
